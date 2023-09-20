302fe134b839c2c9944d6ba825c9b8962d6073e0
LINHSINJU <11010148@gm.chihlee.edu.tw>
2023-09-20 18:52:32 +0800

    修正用法

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -14,36 +14,18 @@ class BooksController extends Controller
 
     public function show(){
         //隨機3個不重複數字
-        if(!session()->has('nums')){ 
-            if(!session()->has('array_called')){
-                $array = arr();
-                session(['array_called' => true]);
-            }else{           
-                $nums = array();
-                while(count($nums)<3){
-                    $num = rand(0,9);
-                    if(!in_array($nums, $num)){
-                        $nums[] = $num;
-                    }
+        if(!session()->has('array_called')){
+            $array = arr();
+            session(['array_called' => true]);
+        }else{           
+            $nums = array();
+            while(count($nums)<3){
+                $num = rand(0,9);
+                if(!in_array($nums, $num)){
+                    $nums[] = $num;
                 }
-                session(['nums'=>$nums]);
-            }            
-        }else{
-            $nums = session('nums', []);
-            //假設這 3 位數用 ABC 排列，如果abs(B-A)=abs(C-B)就停止
-            $abs = abs($nums[1]-$nums[0]);
-            $abs2 = abs($nums[2]-$nums[1]);
-            $n=0;
-            while($n<10){
-                $result = $n."=".$nums;
-                session(['result'=>$result]);
-                if($abs==$abs2){
-                    return view('index',"總共試了10次或以找到數字了喔!");
-                    break;
-                }
-                return view('index',['result' => session('result', [])]);
-            }            
-            return view('index',"總共試了10次或以找到數字了喔!");
+            }
+            session(['nums'=>$nums]);
         }
     }
     
@@ -58,5 +40,23 @@ class BooksController extends Controller
         }
         $array .= ")";
         return view('index',$array);        
+    }
+
+    public function result(){
+        //假設這 3 位數用 ABC 排列，如果abs(B-A)=abs(C-B)就停止
+        $nums = session('nums', []);
+        $abs = abs($nums[1]-$nums[0]);
+        $abs2 = abs($nums[2]-$nums[1]);
+        $n=0;
+        while($n<10){
+            $result = $n."=".$nums;
+            session(['result'=>$result]);
+            if($abs==$abs2){
+                return view('index',['message'=>"總共試了10次或以找到數字了喔!"]);
+                break;
+            }
+            return view('index',['result' => session('result', [])]);
+        }            
+        return view('index',['message'=>"總共試了10次或以找到數字了喔!"]);
     }
 }
