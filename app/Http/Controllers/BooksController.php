d6b293e90960263c28b4dfdda35408256c016930
LINHSINJU <11010148@gm.chihlee.edu.tw>
2023-09-20 17:25:32 +0800

    產生隨機3個不重複數字

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -8,7 +8,23 @@ use Illuminate\Http\Request;
 class BooksController extends Controller
 {
     public function show(){
-        
+        //隨機3個不重複數字
+        if(!session()->has('nums')){
+            $nums = array();
+            while(count($nums)<3){
+                $num = rand(0,9);
+                if(!in_array($nums, $num)){
+                    $nums[] = $num+",";
+                }else{
+                    $nums[] = $num;
+                }
+            }
+            session(['nums'=>$nums]);
+        }else{
+            $nums = session('nums', []);
+        }
+
+
         return view('index');
     }
 }
