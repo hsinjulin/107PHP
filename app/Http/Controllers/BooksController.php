305f60473077a6b90df48db3cf30cc807101c0ce
LINHSINJU <11010148@gm.chihlee.edu.tw>
2023-09-20 17:47:17 +0800

    顯示第一次點選的array

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -10,21 +10,33 @@ class BooksController extends Controller
     public function show(){
         //隨機3個不重複數字
         if(!session()->has('nums')){
-            $nums = array();
-            while(count($nums)<3){
-                $num = rand(0,9);
-                if(!in_array($nums, $num)){
-                    $nums[] = $num+",";
-                }else{
-                    $nums[] = $num;
+            //第一組要顯示array=([0]=>6[1]=>1[2]=>3)
+            if(session()->has('array')){
+                $array = array();
+                $i=0;
+                $array = "Array([";
+                while($i<3){
+                   $array .=  "[".$i."] => ".$nums[$i];
+                   $i++;
                 }
+                $array .= ")";
+                return view('index',['array'=>session('array',[])]);
+            }else{    
+                $nums = array();
+                while(count($nums)<3){
+                    $num = rand(0,9);
+                    if(!in_array($nums, $num)){
+                        $nums[] = $num;
+                    }
+                }
+                session(['nums'=>$nums]);
             }
-            session(['nums'=>$nums]);
         }else{
             $nums = session('nums', []);
         }
-
-
+        
+        
+        
         return view('index');
     }
 }
